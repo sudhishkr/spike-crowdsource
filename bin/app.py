93cd69634766c1f8c27c6757a0e42224264d8c2f
sudhish kasaba ramesh <sudhishr@wepay.com>
2018-08-15 19:16:07 -0700

    fixing

--- bin/app.py
+++ bin/app.py
@@ -1,7 +1,7 @@
 import sys
 from flask import Flask, render_template, request, jsonify
 from flask_restful import Api
-from dbhelper import save_review_2_db
+from dbhelper import save_review, get_review
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,10 +19,10 @@ def home():
 def data_comment():
     if request.method == 'POST':
         json_data = request.get_json(force=True)
-        save_review_2_db(json_data=json_data)
+        save_review(json_data=json_data)
         return jsonify({"status": "success"})
     elif request.method == 'GET':
-        return jsonify({"status": "failed"})
+        return jsonify(get_review())
     else:
         return jsonify({"status": "failed"})
 
--- bin/dbhelper.py
+++ bin/dbhelper.py
@@ -1,11 +1,12 @@
 from database import DataAccess, MongoDB
 
 
-def save_review_2_db(json_data):
+def save_review(json_data):
     data_access_obj = DataAccess()
     data_access_obj.mongodb_obj.add_review(json_data)
     return True
 
 
-def read_review_4m_db():
-    return
+def get_review(self, query_filter={}):
+    data_access_obj = DataAccess()
+    return self.json_out(data_access_obj.mongodb_obj.get_review(query_filter=query_filter))
